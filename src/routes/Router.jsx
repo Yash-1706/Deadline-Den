03b1544c589204f3c46ed5b70b59564a54dfe2a6
Yash-1706 <gaikwad17yash@gmail.com>
2025-04-20 05:45:24 +0530

    Integrated Google Gemini

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Tips from "../pages/Tips";
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/tips" element={<Tips />} />
                 {/* More routes will be added here */}
             </Routes>
         </Router>
